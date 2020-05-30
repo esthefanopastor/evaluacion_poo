b1f5c0648b690c2276ae1844676ff1b8571fbf25
Esthefano Oniel Pastor Palacios <pastorpalaciose@gmail.com>
2020-05-30 18:43:34 -0500

    First commit

---
+++ solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
