b0ed4f4fba827c93c2924057baeafa77ba59f9ea
Esthefano Oniel Pastor Palacios <pastorpalaciose@gmail.com>
2020-05-30 19:18:25 -0500

    Se agregó la clase Norte

---
+++ solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Norte.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres
+{
+    class Norte
+    {
+    }
+}
