d99a0cfbbc9df6a4be16bf62aa86b13e5cdf9cfb
Esthefano Oniel Pastor Palacios <pastorpalaciose@gmail.com>
2020-05-30 21:34:11 -0500

    Se modificó la clase Central

--- solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Central.cs
+++ solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Central.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres
 {
-    class Central
+    public class Central
     {
+        public int NombreMedicamento { get; set; }
+        public int Cantidad { get; set; }
+
+        public virtual double ObtenerSubtotal()
+        {
+            return 1.0;
+        }
+
+        public virtual double ObtenerDescuento()
+        {
+            return 1.0;
+        }
     }
 }
