c8089d5f0a7fbaa905c51e54dff110de0a24cd71
Esthefano Oniel Pastor Palacios <pastorpalaciose@gmail.com>
2020-05-30 21:59:03 -0500

    Elaboración de la clase Central

--- solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Central.cs
+++ solucion_evaluacion/POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres/Central.cs
@@ -11,14 +11,39 @@ namespace POOI_EL1_Esthefano_Pastor_Harold_Mallqui_Luis_Torres
         public int NombreMedicamento { get; set; }
         public int Cantidad { get; set; }
 
-        public virtual double ObtenerSubtotal()
-        {
-            return 1.0;
+        public Central () {
+            Cantidad = 2;
         }
 
-        public virtual double ObtenerDescuento()
+
+        public double ObtenerDescuento()
         {
-            return 1.0;
+            double descuento = 0;
+            if (Cantidad > 5)
+            {
+                descuento = ObtenerSubtotal() * 0.1;
+                return descuento;
+            }
+
+            return descuento;
+        }
+
+        public virtual double ObtenerSubtotal()
+        {
+            double subtotal = 0;
+            switch(NombreMedicamento)
+            {
+                case 0:
+                    subtotal = 1.80;
+                    break;
+                case 1:
+                    subtotal = 13.80;
+                    break;
+                case 2:
+                    subtotal = 9.50;
+                    break;
+            }
+            return subtotal;
         }
     }
 }
